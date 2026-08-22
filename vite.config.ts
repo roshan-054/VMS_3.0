0f3359aa403f0475acf03807e9d8e3537980cada
Roshan <70559805+roshan-054@users.noreply.github.com>
2026-08-22 09:13:40 +0530

    proceed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/VMS_3.0/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
